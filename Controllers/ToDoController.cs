0111bd89d634a5da6854cefb4f5f546d8548c9e6
sasquad92 <krzysztof.sommerrey@gmail.com>
2017-04-05 19:01:51 +0200

    Create, Update and Delete paths were added

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -34,5 +34,57 @@ namespace TodoApi.Controllers
 
             return new ObjectResult(item);
         }
+
+        // POST api/todo
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _todoRepository.Add(item);
+
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        // PUT api/todo/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || id != item.Key)
+            {
+                return BadRequest();
+            }
+
+            var todo = _todoRepository.Find(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _todoRepository.Update(todo);
+            return new NoContentResult();
+        }
+
+        // DELETE api/todo/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _todoRepository.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _todoRepository.Remove(id);
+            return new NoContentResult();
+        }
+
     }
 }
